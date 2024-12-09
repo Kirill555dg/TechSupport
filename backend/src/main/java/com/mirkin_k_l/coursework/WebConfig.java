b9d85220d9dd620e06373f3dd803a53a8f2e0287
Kerrodar <Kirill555dg@gmail.com>
2024-12-10 00:16:02 +0300

    dockerfiles

--- backend/src/main/java/com/mirkin_k_l/coursework/WebConfig.java
+++ backend/src/main/java/com/mirkin_k_l/coursework/WebConfig.java
@@ -1,5 +1,6 @@
 package com.mirkin_k_l.coursework;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Разрешить запросы ко всем эндпоинтам
-                .allowedOrigins("http://localhost:8008") // Разрешить только с указанного источника
+
+        registry.addMapping("/**")// Разрешить запросы ко всем эндпоинтам
+                .allowedOrigins() // Разрешить только с указанного источника
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Разрешить методы
                 .allowedHeaders("*") // Разрешить любые заголовки
                 .allowCredentials(true); // Разрешить отправку куки, если нужно
