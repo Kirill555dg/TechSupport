16543502aae0fbf0902769274d77dbf8ee5783bb
Kerrodar <Kirill555dg@gmail.com>
2025-04-04 08:44:44 +0300

    prepare add sumbodule frontend

--- frontend/babel.config.js
+++
@@ -1,8 +0,0 @@
-const presets = [
-    ['@babel/preset-env', {
-        targets: "defaults",
-        useBuiltIns: "entry"
-    }]
-]
-
-module.exports = { presets };
--- frontend/src/scripts/routes.js
+++
@@ -1,6 +0,0 @@
-const API_URL = "http://backend.localhost"
-const API_APPLICATION_URL = "/api/v1/application"
-const API_AUTH_URL = "/api/v1/auth";
-const API_USER_URL = "/api/v1/user";
-
-export {API_URL, API_APPLICATION_URL, API_USER_URL, API_AUTH_URL};
